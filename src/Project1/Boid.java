656ac4d86f938fbccadc119cfb67c6da6f341bd9
vages <eirikvageskar@gmail.com>
2016-01-31 14:25:30 +0100

    Add separation force, fix division bug and add more parameters.

--- src/Project1/Boid.java
+++ src/Project1/Boid.java
@@ -7,7 +7,10 @@ import java.util.ArrayList;
  */
 public class Boid {
     private static int idCounter;
-    public static double flockingRadius = 50;
+    public static double flockingRadius = 100;
+    public static double separationRadius = 30;
+    public static double separationForceFactor = 2;
+    public static double cohesionFactor = 0.1;
 
     private static float maxSpeed = 10;
     private final Vector worldDimensions;
@@ -37,10 +40,38 @@ public class Boid {
 
         Vector align = calculateAlignmentForce(neighbours);
         Vector cohere = calculateCohesionForce(neighbours);
+        Vector separation = calculateSeparationForce(neighbours);
 
-        this.velocity = this.velocity.plus(align).plus(cohere);
+        this.velocity = this.velocity.plus(align);
+        this.velocity = this.velocity.plus(cohere);
+        this.velocity = this.velocity.plus(separation);
+        this.velocity = this.velocity.limit(maxSpeed);
+
         this.position = this.position.plus(this.velocity);
         this.position = this.position.elementWiseModulo(this.worldDimensions);
+    }
+
+    private Vector calculateSeparationForce(ArrayList<Boid> neighbours) {
+        Vector sumOfPositions = new Vector(new double[]{0, 0});
+
+        if (neighbours.size() == 0){
+            return sumOfPositions;
+        }
+
+        for (Boid n: neighbours) {
+
+            Vector diff = n.getPosition().minus(this.getPosition());
+
+            int distance = diff.length();
+            if (distance < Boid.separationRadius) {
+                diff = diff.direction();
+                diff = diff.times(separationForceFactor /Math.pow(distance, 2));
+                sumOfPositions = sumOfPositions.minus(diff);
+            }
+
+        }
+
+        return sumOfPositions;
     }
 
     private Vector calculateCohesionForce(ArrayList<Boid> neighbours) {
@@ -54,9 +85,11 @@ public class Boid {
             sumOfPositions = sumOfPositions.plus(n.getPosition());
         }
 
-        sumOfPositions = sumOfPositions.times(1/neighbours.size());
+        sumOfPositions = sumOfPositions.times(1.0/neighbours.size());
 
-        return sumOfPositions.minus(this.getPosition()).times(0.01); // Must be prettified
+        Vector d_v = sumOfPositions.minus(this.getPosition());
+
+        return d_v.times(cohesionFactor); // Must be prettified
     }
 
     private Vector calculateAlignmentForce(ArrayList<Boid> neighbours) {
