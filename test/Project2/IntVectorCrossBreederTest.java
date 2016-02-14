f0102ca6cafa42e8d5f7f28a80ff5fdf0784fcea
vages <eirikvageskar@gmail.com>
2016-02-14 17:47:27 +0100

    Move tests to separate directory

---
+++ test/Project2/IntVectorCrossBreederTest.java
@@ -0,0 +1,26 @@
+package Project2;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class IntVectorCrossBreederTest {
+
+    @Test
+    public void testCrossBreed() throws Exception {
+        OneMaxIndividual a = new OneMaxIndividual(new int[]{0, 0, 0, 0, 0});
+        OneMaxIndividual b = new OneMaxIndividual(new int[]{1, 1, 1, 1, 1});
+
+        IntVectorCrossBreeder cb = new IntVectorCrossBreeder(new Random(0)); // Seed so that crossing point is index 3
+
+        int[][] children = cb.crossBreed(a, b);
+        int[] childAGenotype = children[0];
+        int[] childBGenotype = children[1];
+
+        assertArrayEquals(new int[]{0, 0, 0, 1, 1}, childAGenotype);
+        assertArrayEquals(new int[]{1, 1, 1, 0, 0}, childBGenotype);
+    }
+}
