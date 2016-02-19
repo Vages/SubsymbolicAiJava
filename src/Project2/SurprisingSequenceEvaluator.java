df5ed5bce0080332dcc96fa9e7833d2090a357f7
vages <eirikvageskar@gmail.com>
2016-02-19 23:19:35 +0100

    Add a SurprisingSequenceEvaluator

---
+++ src/Project2/SurprisingSequenceEvaluator.java
@@ -0,0 +1,37 @@
+package Project2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SurprisingSequenceEvaluator implements PhenotypeEvaluator {
+    private boolean globallySurprising;
+
+    public SurprisingSequenceEvaluator(boolean globallySurprising) {
+        this.globallySurprising = globallySurprising;
+    }
+
+    @Override
+    public double evaluate(int[] phenotype) {
+        double errors = 0;
+
+        int maxDist = (globallySurprising) ? phenotype.length-1 : 1;
+
+        HashSet<Tuple> alreadySeen = new HashSet<>();
+
+        for (int i = 1; i <= maxDist; i++) {
+            alreadySeen.clear();
+
+            for (int j = 0; j < phenotype.length-i; j++) {
+                int a = phenotype[j];
+                int b = phenotype[j+i];
+                Tuple sequence = new Tuple(a, b);
+                if (alreadySeen.contains(sequence)) {
+                    errors++;
+                }
+                alreadySeen.add(sequence);
+            }
+        }
+
+        return 1.0/(1.0+errors);
+    }
+}
---
+++ test/Project2/SurprisingSequenceEvaluatorTest.java
@@ -0,0 +1,41 @@
+package Project2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SurprisingSequenceEvaluatorTest {
+
+    @Test
+    public void testEvaluate() throws Exception {
+        SurprisingSequenceEvaluator locallySurprising = new SurprisingSequenceEvaluator(false);
+        SurprisingSequenceEvaluator globallySurprising = new SurprisingSequenceEvaluator(true);
+
+        int[] abccba = {0, 1, 2, 2, 1, 0};
+        int[] aabcc = {0,0,1,2,2};
+        int[] abbacca = {0,1,1,0,2,2,0};
+        int[] abcbc = {0,1,2,1,2};
+
+        // ABCCBA should be surprising globally and locally
+
+        assertEquals(1.0, locallySurprising.evaluate(abccba), 0.001);
+        assertEquals(1.0, globallySurprising.evaluate(abccba), 0.001);
+
+
+        // AABCC should be locally surprising, but not globally
+
+        assertEquals(1.0, locallySurprising.evaluate(aabcc), 0.001);
+        assertNotEquals(1.0, globallySurprising.evaluate(aabcc), 0.001);
+
+        // ABBACCA should be locally surprising, but not globally
+
+        assertEquals(1.0, locallySurprising.evaluate(abbacca), 0.001);
+        assertNotEquals(1.0, globallySurprising.evaluate(abbacca), 0.001);
+
+        // ABCBC should be surprising neither globally nor locally
+
+        assertNotEquals(1.0, locallySurprising.evaluate(abcbc), 0.001);
+        assertNotEquals(1.0, globallySurprising.evaluate(abcbc), 0.001);
+
+    }
+}
