ff2fae88d47a1de85c0861318f82c2d234df52a6
vages <eirikvageskar@gmail.com>
2016-04-03 13:11:21 +0200

    Start with poisonsensings. Refactor a little.

--- src/main/java/project3/Board.java
+++ src/main/java/project3/Board.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Board {
     private int boardSize = 10;
-    private static Map<Heading, Map<MoveDirection, int[]>> directionToSensorAndMoveCoordinates;
+    private static final Map<Heading, Map<MoveDirection, int[]>> directionToSensorAndMoveCoordinates;
+    private static final MoveDirection[] mdIterable = {MoveDirection.LEFT, MoveDirection.FORWARD, MoveDirection.RIGHT};
     private int[] playerPosition = new int[2];
     private int[] startingCell = new int[2];
     private Heading playerHeading;
@@ -15,16 +16,15 @@ public class Board {
         // Fill the Map with the right heading/direction mappings
 
         directionToSensorAndMoveCoordinates = new HashMap<>();
-        int[][] relativeCoordinates = new int[][] {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-        MoveDirection[] dirs = {MoveDirection.LEFT, MoveDirection.FORWARD, MoveDirection.RIGHT};
+        int[][] relativeCoordinates = new int[][]{{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
         Heading[] heads = {Heading.NORTH, Heading.WEST, Heading.SOUTH, Heading.EAST};
 
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             Heading h = heads[i];
             Map<MoveDirection, int[]> this_hs_map = new HashMap<>();
-            for (int j = 0; j < 3; j++){
+            for (int j = 0; j < 3; j++) {
                 int i1 = (i + j + 4 - 1) % 4;
-                this_hs_map.put(dirs[j], relativeCoordinates[i1]);
+                this_hs_map.put(mdIterable[j], relativeCoordinates[i1]);
             }
             directionToSensorAndMoveCoordinates.put(h, this_hs_map);
         }
@@ -33,8 +33,8 @@ public class Board {
     public Board(double f, double p, int[] startingCell) {
 
         // Fill the board with poison with the given food-poison-distribution
-        for (int i = 0; i<boardSize; i++){
-            for (int j = 0; j<boardSize; j++){
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
                 if (i == 0 && j == 0) {
                     continue; // This space will be used as the player space
                 }
@@ -53,24 +53,26 @@ public class Board {
         playerHeading = Heading.SOUTH;
     }
 
-    public CellType getCell(int x, int y){
-        return this.grid[(y+boardSize)%boardSize][(x+boardSize)%boardSize];
+    public CellType getCell(int x, int y) {
+        return this.grid[(y + boardSize) % boardSize][(x + boardSize) % boardSize];
     }
 
-    public CellType move(MoveDirection d){
+    public CellType move(MoveDirection d) {
         return null;
     }
 
-    public boolean[] sensePoison(){
-        return null;
+    public boolean[] sensePoison() {
+        boolean[] poisonSensings = new boolean[3];
+
+        return poisonSensings;
     }
 
-    public boolean[] senseFood(){
+    public boolean[] senseFood() {
         return null;
     }
 
     public static void main(String[] args) {
-        Board a = new Board(0.33, 0.33, new int[]{0,0});
+        Board a = new Board(0.33, 0.33, new int[]{0, 0});
         System.out.println("hello");
     }
 }
