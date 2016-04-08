e433d4f51c4b8578a33c090a60be47dd26755670
vages <eirikvageskar@gmail.com>
2016-04-08 18:57:26 +0200

    Add some methods for getting occupied cells

--- src/main/java/project4/BeerTrackerGame.java
+++ src/main/java/project4/BeerTrackerGame.java
@@ -1,25 +1,41 @@
 package project4;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BeerTrackerGame {
     private int width;
     private int height;
     private int trackerPosition = 0;
-    private int maxTrackerPosition;
     private int trackerWidth;
+    private int fallingObjectXPosition;
+    private int fallingObjectYPosition;
 
     public BeerTrackerGame(int width, int height, int trackerWidth) {
         this.width = width;
         this.height = height;
         this.trackerWidth = trackerWidth;
-        this.maxTrackerPosition = width-trackerWidth;
+        spawnNewFallingObject();
     }
 
-    public void performAction(TrackerAction a, int magnitude){
+    /**
+     * Performs an action requested by the player
+     *
+     * @param a action to be performed
+     * @param magnitude the magnitude of the action (used in conjunction with moving left and right)
+     */
+    public void performAction(TrackerAction a, int magnitude) {
         if (a == TrackerAction.MOVE_LEFT || a == TrackerAction.MOVE_RIGHT) {
             moveTracker(a, magnitude);
         }
     }
 
+    /**
+     * Moves the tracker a given number of steps.
+     *
+     * @param a direction
+     * @param magnitude number of steps to move
+     */
     private void moveTracker(TrackerAction a, int magnitude) {
         if (a == TrackerAction.MOVE_LEFT) {
             trackerPosition -= magnitude;
@@ -27,7 +43,37 @@ public class BeerTrackerGame {
             trackerPosition += magnitude;
         }
 
-        // Modulo operation to ensure wraparond
+        // Modulo operation to ensure wraparound
         trackerPosition = (trackerPosition + width) % width;
+    }
+
+    /**
+     * Returns a set of the horizontal positions currently occupied by an object at the given starting position and of the given size.
+     *
+     * @param startingPosition x-position of the object
+     * @param size the size of the object
+     * @return set of x-positions occupied by the object
+     */
+    public Set<Integer> getCellsOccupiedByObject(int startingPosition, int size) {
+        Set<Integer> occupiedCells = new HashSet<>();
+
+        for (int i = 0; i < size; i++) {
+            occupiedCells.add((startingPosition + i) % width);
+        }
+
+        return occupiedCells;
+    }
+
+    /**
+     * Spawn a new falling object at the top of the screen.
+     */
+    private void spawnNewFallingObject() {
+        fallingObjectYPosition = height;
+        fallingObjectXPosition = 1 + (int) (Math.random()*6); // A random number from 1 to 6
+    }
+
+    public boolean[] getShadowSensings(){
+        boolean[] sensorReadings = new boolean[trackerWidth];
+        return null;
     }
 }
---
+++ src/test/java/project4/BeerTrackerGameTest.java
@@ -0,0 +1,31 @@
+package project4;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class BeerTrackerGameTest {
+
+    @Test
+    public void testGetCellsOccupiedByObject() throws Exception {
+        // Test without wraparound
+        BeerTrackerGame g = new BeerTrackerGame(30, 15, 5);
+
+        Set<Integer> expectedSimpleResults = new HashSet<>();
+        Integer[] simpleElements = {0, 1, 2, 3, 4};
+        Collections.addAll(expectedSimpleResults, simpleElements);
+
+        assertEquals(expectedSimpleResults, g.getCellsOccupiedByObject(0, 5));
+
+        // Test with wraparound
+        Set<Integer> expectedAdvancedResults = new HashSet<>();
+        Integer[] advancedElements = {0, 1, 2, 28, 29};
+        Collections.addAll(expectedAdvancedResults, advancedElements);
+
+        assertEquals(expectedAdvancedResults, g.getCellsOccupiedByObject(28, 5));
+    }
+}
