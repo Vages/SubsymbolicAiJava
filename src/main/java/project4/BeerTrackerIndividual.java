d17716f60958f2a68e97940ca544158d1cfcc310
vages <eirikvageskar@gmail.com>
2016-04-08 14:02:48 +0200

    Make method for generating random genotype.

---
+++ src/main/java/project4/BeerTrackerIndividual.java
@@ -0,0 +1,50 @@
+package project4;
+
+import java.util.ArrayList;
+
+public class BeerTrackerIndividual {
+
+    public static NeuralNetworkGene[] generateRandomGenotype(int[] topology) {
+        ArrayList<NeuralNetworkGene> genes = new ArrayList<>();
+
+        for (int i = 1; i < topology.length; i++) {
+            int neuronsInThisLayer = topology[i];
+            int neuronsInPreviousLayer = topology[i - 1];
+            addGenesForLayer(genes, neuronsInThisLayer, neuronsInPreviousLayer);
+        }
+
+        return (NeuralNetworkGene[]) genes.toArray();
+    }
+
+    private static void addGenesForLayer(ArrayList<NeuralNetworkGene> genes, int neuronsInThisLayer, int neuronsInPreviousLayer) {
+        int normalWeightsForThisLayer = neuronsInThisLayer * (neuronsInThisLayer + neuronsInPreviousLayer);
+
+        // Normal weights
+        for (int j = 0; j < normalWeightsForThisLayer; j++) {
+            NeuralNetworkGene gene = new NeuralNetworkGene(0, -5, 5);
+            gene.mutate();
+            genes.add(gene);
+        }
+
+        // Biases
+        for (int j = 0; j < neuronsInThisLayer; j++) {
+            NeuralNetworkGene gene = new NeuralNetworkGene(0, -10, 0);
+            gene.mutate();
+            genes.add(gene);
+        }
+
+        // Gains
+        for (int j = 0; j < neuronsInThisLayer; j++) {
+            NeuralNetworkGene gene = new NeuralNetworkGene(0, 1, 5);
+            gene.mutate();
+            genes.add(gene);
+        }
+
+        // Time constants
+        for (int j = 0; j < neuronsInThisLayer; j++) {
+            NeuralNetworkGene gene = new NeuralNetworkGene(0, 1, 2);
+            gene.mutate();
+            genes.add(gene);
+        }
+    }
+}
