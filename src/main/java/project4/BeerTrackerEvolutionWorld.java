291d834be5b51a5bc65541f56b7d3e03285a8edc
vages <eirikvageskar@gmail.com>
2016-04-11 00:50:01 +0200

    Update rewards to only award points for complete captures.

--- src/main/java/project4/BeerTrackerEvolutionWorld.java
+++ src/main/java/project4/BeerTrackerEvolutionWorld.java
@@ -24,10 +24,10 @@ public class BeerTrackerEvolutionWorld extends EvolutionWorld<NeuralNetworkGene>
 
         rewards = new HashMap<>();
         rewards.put(GameEvent.CAPTURED_SMALL, 10.0);
-        rewards.put(GameEvent.PARTIALLY_CAPTURED_SMALL, 1.0);
+        rewards.put(GameEvent.PARTIALLY_CAPTURED_SMALL, 0.0);
         rewards.put(GameEvent.AVOIDED_SMALL, 0.0);
         rewards.put(GameEvent.CAPTURED_BIG, 10.0);
-        rewards.put(GameEvent.PARTIALLY_CAPTURED_BIG, 1.0);
+        rewards.put(GameEvent.PARTIALLY_CAPTURED_BIG, 0.0);
         rewards.put(GameEvent.AVOIDED_BIG, 0.0);
         rewards.put(GameEvent.NOTHING, 0.0);
         rewards.put(GameEvent.GAME_OVER, 0.0);
