c42e4a926c179408dc963a017f1e20304e9a4fc7
vages <eirikvageskar@gmail.com>
2016-04-11 20:37:09 +0200

    Add test for creating matrices

--- src/test/java/project3/MatrixAndVectorOperationsTest.java
+++ src/test/java/project3/MatrixAndVectorOperationsTest.java
@@ -3,6 +3,7 @@ package project3;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static project3.MatrixAndVectorOperations.createTwoDimensionalMatrix;
 import static project3.MatrixAndVectorOperations.multiplyVectorByMatrix;
 
 public class MatrixAndVectorOperationsTest {
@@ -14,5 +15,13 @@ public class MatrixAndVectorOperationsTest {
         double[] expected = {2*67, 2*70, 2*49};
 
         assertArrayEquals(expected, multiplyVectorByMatrix(simpleVector, bigMatrix), 0.1);
+    }
+
+    @Test
+    public void testCreateTwoDimensionalMatrix() throws Exception {
+        double[][] expected = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+        double[] data = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        assertArrayEquals(expected, createTwoDimensionalMatrix(data, 2, 4));
     }
 }
