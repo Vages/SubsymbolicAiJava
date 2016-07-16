9591deb97e5b0362fd85f0ff6cfb136b544b36ff
Vages <eirikvageskar@gmail.com>
2016-07-16 17:38:01 +0200

    Add the last stuff.

---
+++ src/main/java/project5/TSPIndividual.java
@@ -0,0 +1,19 @@
+package project5;
+
+public class TSPIndividual {
+    public Integer[] getGenotype() {
+        return new Integer[0];
+    }
+
+    public double getFitness() {
+        return 0;
+    }
+
+    public void develop() {
+
+    }
+
+    public void assessFitness() {
+
+    }
+}
